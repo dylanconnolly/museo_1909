f2bf8dc70ee728dbe04be6abec2d90b591687424
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-30 09:28:22 -0600

    Create add photographs and add artists method to curator class, add tests for methods to test file

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -8,6 +8,36 @@ class CuratorTest < Minitest::Test
 
   def setup
     @curator = Curator.new
+
+    @photo_1 = Photograph.new({
+     id: "1",
+     name: "Rue Mouffetard, Paris (Boy with Bottles)",
+     artist_id: "1",
+     year: "1954"
+     })
+
+    @photo_2 = Photograph.new({
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+      })
+
+    @artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+      })
+
+    @artist_2 = Artist.new({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+      })
   end
 
   def test_it_exists
@@ -19,4 +49,22 @@ class CuratorTest < Minitest::Test
     assert_equal [], curator.photographs
   end
 
+  def test_photographs_can_be_added_to_curator
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_curator_initializes_with_no_artists
+    curator = Curator.new
+    assert_equal [], curator.artists
+  end
+
+  def test_artists_can_be_added_to_curator
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
 end
