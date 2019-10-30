9ee7aa40db23d91058905082918b25b5aada5e7f
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-30 10:19:14 -0600

    Create load_photographs method in FileIO class

--- lib/file_io.rb
+++ lib/file_io.rb
@@ -1,5 +1,12 @@
-require 'csv'
+require './lib/photograph'
+require './lib/artist'
+require 'CSV'
 
 class FileIO
 
+  def load_photographs(file_path)
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      add_photograph(Photograph.new(row))
+    end
+  end
 end
